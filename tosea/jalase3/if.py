2797ad9e413391d1243ba4f0d7205a5ff974343a
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-11-16 14:39:35 +0330

    change

---
+++ tosea/jalase3/if.py
@@ -0,0 +1,19 @@
+# دستورات شرطی
+
+weather = "rainy"
+
+if(weather == "suuny"):
+  print("برو مدرسه")
+else:
+  print("نرو مدرسه")
+
+avg = 16.79
+
+if(avg >= 12):
+  print("قبول شدی")
+else:
+  print("قبول نشدی")
+
+sen = 10
+
+if(sen < 10):

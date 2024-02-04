c8bda10943435a89155b6b08d32b029523626aa7
reza gholamalitabar <reza.gh.fi@gmail.com>
2024-02-04 13:20:31 +0330

    jalase11 tosea

---
+++ tosea/jalase11/tamrin2.py
@@ -0,0 +1,13 @@
+#list
+nomarat = [20, 19, 16, 20, 18, 20, 17, 18, 20, 20, 19, 20, 20, 20]
+
+jam = 0
+
+for x in nomarat:
+  jam = jam + x
+
+moadel = jam / 14
+
+print(moadel)
+
+  
---
+++ tosea/jalase11/wow.py
@@ -0,0 +1,36 @@
+def wow(n):
+  pass
+
+def wow2(n):
+  output = "w"
+  if(n <= 0 or n>10):
+    return "wrong"
+  while(n > 0):
+    output += "o"
+    n = n - 1
+  output += "w!"
+  return output
+print(wow2(11))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n =1 
+m =1
+d = 1
+
+
+def hasan():
+    pass
+    n = 1
+
